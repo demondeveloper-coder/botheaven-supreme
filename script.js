0a515092e628e02d187cef3461cd97688e3985a3
demondeveloper-coder <112100721+demondeveloper-coder@users.noreply.github.com>
2023-04-01 23:01:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function fire() {
+
+  location = 'https://discord.gg/MR5RDU7f4m';
+
+}
+
+function bi() {
+
+  location = 'https://discord.com/oauth2/authorize?client_id=1016193839016443990&scope=bot&permissions=1071862767553';
+
+} 
+
+function app() {
+
+  location = 'https://apk.e-droid.net/apk/app2229021-wxacnz.apk?v=6'
+
+}
